2b78a30efaf937abb35cffafd6175539982e64b1
Hemant Kumar <hemant@example.com>
2026-03-12 09:52:46 +0530

    test ci cd process

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,7 +9,7 @@ from main import app
 @pytest.mark.asyncio
 @pytest.mark.parametrize("username", ["octocat", "hemuku90"])
 async def test_get_user_gists_success(username):
-    """Test successful retrieval of user gists from real GitHub API."""
+    """Test successful retrieval of user gists from real GitHub API. Test CI-CD"""
     async with AsyncClient(
         transport=ASGITransport(app=app), base_url="http://test"
     ) as client:
